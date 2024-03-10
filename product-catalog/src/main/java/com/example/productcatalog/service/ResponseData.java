2e9613811345d889761b7f5a0769f873be6a4218
chamixEranda <chamitheranda00@gmail.com>
2024-03-10 10:38:30 +0530

    add new response data & supply management

--- product-catalog/src/main/java/com/example/productcatalog/service/ResponseData.java
+++ product-catalog/src/main/java/com/example/productcatalog/service/ResponseData.java
@@ -1,2 +1,41 @@
-package com.example.productcatalog.service;public class ResponseData {
+package com.example.productcatalog.service;
+
+public class ResponseData<Product> {
+
+    private boolean status;
+    private String message;
+    private Product data;
+
+    public ResponseData() {
+    }
+
+    public ResponseData(boolean status, String message, Product data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Product getData() {
+        return data;
+    }
+
+    public void setData(Product data) {
+        this.data = data;
+    }
 }
